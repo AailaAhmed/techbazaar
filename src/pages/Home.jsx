e2c8178ce4c4402f59d229743aefdfdbf7fbd691
Aaila Ahmed <aailaahmed@Aailas-MacBook-Air.local>
2026-09-15 18:02:54 +0500

    Polish for tighter layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ function Home(){
     return(
         <div>
             <section className="bg-gradient-to-r from-[#436EDF] to-blue-200 text-white">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-20 lg:py-28 text-center">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-20 lg:py-28 text-center">
                     <h1 className="text-xl sm:text-3xl lg:text-6xl text-[#F8FAE5] font-bold">
                         Discover the latest tech
                     </h1>
