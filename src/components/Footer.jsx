aef34eed34ea9db516347d1c928b615500421f2a
Aaila Ahmed <aailaahmed@Aailas-MacBook-Air.local>
2026-09-15 18:12:47 +0500

    Polish for tighter layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ function Footer(){
             </div>
          </div>
 
-         <div className="border-t border-gray-700 mt-10 pt-6 text-center">
+         <div className="border-t border-gray-700 mt-10 mb-4 pt-6 text-center">
             <p className="text-xs sm:text-sm">© 2026 TechBazaar. All rights reserved.</p>
 
          </div>
