3b1274a102521a7700c303a552f8f274d7225ddf
Aaila Ahmed <aailaahmed@Aailas-MacBook-Air.local>
2026-09-15 13:28:14 +0500

    Polish for tighter layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,12 +75,12 @@ function Home(){
                 </div>
             </section>
 
-            <section className="py-8">
+            <section className="py-6">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <h2 className="text-xl font-bold text-gray-900">Categories</h2>
                     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
                         {categories.map((cat)=>(
-                            <Link key={cat.value} to={`/shop?category=${cat.value}`} className="bg-gray-100 mt-4 rounded-xl p-4 sm:p-2 text-center hover:bg-gray-200 transition"> 
+                            <Link key={cat.value} to={`/shop?category=${cat.value}`} className="bg-gray-100 mt-4 rounded-xl p-2 sm:p-2 text-center hover:bg-gray-200 transition"> 
                                <p className="font-semibold text-[#0D0D7B]">{cat.name}</p>
                             </Link>
                         ))}
@@ -88,7 +88,7 @@ function Home(){
                 </div>
             </section>
 
-            <section className="py-16 bg-gray-100">
+            <section className="py-8 bg-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <h2 className="text-xl font-bold text-gray-900 mb-4">
                         Featured Products
@@ -119,7 +119,7 @@ function Home(){
                 </div>
             </section>
 
-            <section className="py-16">
+            <section className="py-8">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <h2 className="text-xl font-bold text-gray-900 mb-4">
                         Best Sellers
@@ -127,7 +127,7 @@ function Home(){
                     {loading && <p>Loading...</p>}
                     {!loading && error && <p>{error}</p>}
                     {!loading && !error && (
-                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
+                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-6 lg:gap-8">
                             {bestSellers.map((product)=> (
                                 <ProductCard 
                                   key={product.id}
@@ -144,7 +144,7 @@ function Home(){
                 </div>
             </section>
 
-            <section className="py-16 bg-gradient-to-r from-[#436EDF] to-blue-200 text-white">
+            <section className="py-8 bg-gradient-to-r from-[#436EDF] to-blue-200 text-white">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <h2 className="text-xl font-bold text-gray-900 mb-4">
                         Flash Sale
